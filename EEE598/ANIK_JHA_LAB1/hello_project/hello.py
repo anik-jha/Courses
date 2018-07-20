1fea8b509b187a42d640965c7eadb60dafd49717
Anik Jha <37495216+anik-jha@users.noreply.github.com>
2018-07-20 13:48:10 -0700

    error files

--- EEE598/ANIK_JHA_LAB1/hello_project/hello.py
+++
@@ -1,4 +0,0 @@
-print("Hello World!") # print to console
-bar = 4
-foo =bar/2
-print("Done")
--- EEE598/ANIK_JHA_LAB3/ANIK_JHA_LAB3/layers/__init__.py
+++
@@ -1,6 +0,0 @@
-from full import FullLayer
-from relu import ReluLayer
-from softmax import SoftMaxLayer
-from cross_entropy import CrossEntropyLayer
-from sequential import Sequential
-from sequential_better import Sequential_better
